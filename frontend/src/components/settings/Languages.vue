2fc7278a9b2c1a9cbdfe23278f74dae149c613bb
Laurynas Gadliauskas <laurynas.gadliauskas@hostinger.com>
2026-01-15 12:32:24 +0200

    fix: adjust logout action order

--- frontend/src/components/settings/Languages.vue
+++ frontend/src/components/settings/Languages.vue
@@ -15,21 +15,21 @@ export default {
   data() {
     const dataObj = {};
     const locales = {
-      "ar_AR": "العربية",
-      "en_GB": "English",
-      "es_AR": "Español (Argentina)",
-      "es_CO": "Español (Colombia)",
-      "es_ES": "Español",
-      "es_MX": "Español (Mexico)",
-      "fr_FR": "Français",
-      "id_ID": "Bahasa Indonesia",
-      "lt_LT": "Lietuvių",
-      "pt_BR": "Português (Brasil)",
-      "pt_PT": "Português",
-      "ru_RU": "Русский",
-      "tr_TR": "Türkçe",
-      "uk_UA": "Український",
-      "zh_CN": "中文 (简体)",
+      ar_AR: "العربية",
+      en_GB: "English",
+      es_AR: "Español (Argentina)",
+      es_CO: "Español (Colombia)",
+      es_ES: "Español",
+      es_MX: "Español (Mexico)",
+      fr_FR: "Français",
+      id_ID: "Bahasa Indonesia",
+      lt_LT: "Lietuvių",
+      pt_BR: "Português (Brasil)",
+      pt_PT: "Português",
+      ru_RU: "Русский",
+      tr_TR: "Türkçe",
+      uk_UA: "Український",
+      zh_CN: "中文 (简体)",
     };
 
     // Vue3 reactivity breaks with this configuration
